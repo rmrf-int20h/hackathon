a6c98e3d76d26f574879dd109f0ee6ac94735170
Mykola Lev <mlev@disroot.org>
2019-02-23 18:29:15 +0200

    update settings.py

--- hackapp/hackapp/settings.py
+++ hackapp/hackapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7r1#1i&00(h3zclphn#k*%%(qj#f+k+$60j)g-rwsl=bl!fl1w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
